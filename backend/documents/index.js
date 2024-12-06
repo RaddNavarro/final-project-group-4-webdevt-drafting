91cf7e57b007645a1c14a2cf7f01f4c969ed1a79
RaddNavarro <radian_navarro@dlsl.edu.ph>
2024-12-06 22:24:06 +0800

    test2

---
+++ backend/documents/index.js
@@ -0,0 +1,10 @@
+module.exports = ({ id, email, firstName, grossPay, basicSalary, deductions, netPay, overtimePay }) => {
+    const today = new Date();
+
+return `
+    <!doctype html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+`
+}
